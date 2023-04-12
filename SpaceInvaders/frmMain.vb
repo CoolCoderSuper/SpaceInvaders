3ecb491128bbb3068d3ee6a443fe760884df25ab
Joseph Guenther <joseph@codingcool.ca>
2023-04-11 20:58:47 -0400

    Initial commit

---
+++ SpaceInvaders/frmMain.vb
@@ -0,0 +1,271 @@
+﻿Public Class frmMain
+    Public playerSpeed As Integer = 6
+    Public bombSpeed As Integer = 10
+    Public invaderSpeed As Integer = 5
+    Public reloads As Integer = 3
+    Private bullets As Integer = 30
+    Private goleft As Boolean
+    Private goright As Boolean
+    Private score As Integer = 0
+    Private isPressed As Boolean
+    Private totalEnemies As Integer = 19
+    Private isFriendHere As Boolean = False
+    Private allowBomb As Boolean = False
+
+    Private Sub KeyDown_CMD(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.KeyCode = Keys.Left Then
+            goleft = True
+        End If
+        If e.KeyCode = Keys.Right Then
+            goright = True
+        End If
+        If e.KeyCode = Keys.Space AndAlso Not isPressed Then
+            If Not (lblScore.Text.Contains("Game Over") Or lblScore.Text.Contains("You Win")) Then
+                isPressed = True
+                makeBullet()
+                bullets -= 1
+            End If
+        End If
+        If e.KeyCode = Keys.R Then
+            If Not reloads <= 0 Then
+                reloads -= 1
+                bullets = 30
+            Else
+                gameOver(OverMethod.AMMO)
+            End If
+        End If
+    End Sub
+
+    Private Sub KeyUp_CMD(sender As Object, e As KeyEventArgs) Handles MyBase.KeyUp
+        If e.KeyCode = Keys.Left Then
+            goleft = False
+        End If
+        If e.KeyCode = Keys.Right Then
+            goright = False
+        End If
+        If isPressed Then
+            isPressed = False
+        End If
+    End Sub
+
+    Private Sub tmrGame_Tick(sender As Object, e As EventArgs) Handles tmrGame.Tick
+        If goleft Then
+            pbMe.Left -= playerSpeed
+        ElseIf goright Then
+            pbMe.Left += playerSpeed
+        End If
+
+        If score > 10 Then
+            Dim r As New Random
+            Dim num As Integer = r.Next(100)
+            If num = 7 Then
+                If Not isFriendHere Then
+                    makeFriend()
+                    isFriendHere = True
+                End If
+            End If
+        End If
+        If isFriendHere Then
+            For Each i As Control In Controls
+                If TypeOf i Is PictureBox AndAlso i.Tag Is "friend" Then
+                    CType(i, PictureBox).Left += invaderSpeed
+                    If CType(i, PictureBox).Left > Width + 20 Then
+                        CType(i, PictureBox).Left = 20
+                    End If
+                End If
+            Next
+        End If
+
+        lblScore.Text = "Score : " & score
+        If score > totalEnemies - 1 Then
+            gameOver(OverMethod.WIN)
+        End If
+        If bullets < 0 Then
+            lblBullets.Text = "0"
+        Else
+            lblBullets.Text = bullets
+        End If
+    End Sub
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        tmrGame.Start()
+        tmrBombMaker.Start()
+        tmrGuns.Start()
+        tmrCollision.Start()
+        tmrShields.Start()
+    End Sub
+
+    Private Sub makeBullet()
+        If Not bullets <= 0 Then
+            Dim bullet As PictureBox = New PictureBox
+            bullet.BackColor = Color.Black
+            bullet.Size = New Size(5, 20)
+            bullet.Tag = "bullet"
+            bullet.Left = pbMe.Left + pbMe.Width / 2
+            bullet.Top = pbMe.Top - 20
+            Controls.Add(bullet)
+            bullet.BringToFront()
+        End If
+    End Sub
+
+    Private Sub makeBomb()
+        Dim r As New Random
+        Dim bomb As PictureBox = New PictureBox
+        bomb.BackColor = Color.Orange
+        bomb.Size = New Size(5, 20)
+        bomb.Tag = "bomb"
+        bomb.Left = r.Next(0, Width)
+        bomb.Top = Top + 10
+        Controls.Add(bomb)
+        bomb.BringToFront()
+    End Sub
+
+    Private Sub makeFriend()
+        Dim pb As PictureBox = New PictureBox
+        pb.Size = New Size(50, 50)
+        pb.Tag = "friend"
+        pb.BackColor = Color.Aqua
+        pb.Location = New Point(535, 12)
+        Controls.Add(pb)
+        pb.BringToFront()
+    End Sub
+
+    Private Sub gameOver(method As OverMethod)
+        If method = OverMethod.LOSE Then
+            lblScore.Text &= " Game Over. You been killed by the aliens!!!"
+        ElseIf method = OverMethod.WIN Then
+            lblScore.Text &= " You Win!!!"
+        ElseIf method = OverMethod.AMMO Then
+            lblScore.Text &= " Game Over. You're out of ammo!!!"
+        ElseIf method = OverMethod.MURDERER Then
+            lblScore.Text &= " Game Over. You killed the friend!!!"
+        End If
+        removeBullets()
+        tmrGame.Stop()
+        tmrBombMaker.Stop()
+        tmrGuns.Stop()
+        tmrCollision.Stop()
+    End Sub
+
+    Private Sub removeBullets()
+        For Each j As Control In Controls
+            If TypeOf j Is PictureBox AndAlso j.Tag Is "bullet" Then
+                Controls.Remove(j)
+            End If
+        Next
+    End Sub
+
+    Private Sub tmrBombMaker_Tick(sender As Object, e As EventArgs) Handles tmrBombMaker.Tick
+        makeBomb()
+    End Sub
+
+    Private Sub tmrGuns_Tick(sender As Object, e As EventArgs) Handles tmrGuns.Tick
+        For Each y As Control In Controls
+            If TypeOf y Is PictureBox AndAlso y.Tag Is "bullet" Then
+                y.Top -= 20
+                If CType(y, PictureBox).Top < Height - (Height - 10) Then
+                    Controls.Remove(y)
+                End If
+            End If
+        Next
+        For Each y As Control In Controls
+            If TypeOf y Is PictureBox AndAlso y.Tag Is "bomb" Then
+                y.Top += bombSpeed
+                If Not DesktopBounds.IntersectsWith(y.Bounds) Then
+                    Controls.Remove(y)
+                End If
+            End If
+        Next
+        For Each i As Control In Controls
+            If TypeOf i Is PictureBox AndAlso i.Tag Is "bomb" Then
+                If i.Bounds.IntersectsWith(pbMe.Bounds) Then
+                    Controls.Remove(i)
+                    gameOver(OverMethod.LOSE)
+                    Exit Sub
+                End If
+            End If
+        Next
+    End Sub
+
+    Private Sub tmrCollision_Tick(sender As Object, e As EventArgs) Handles tmrCollision.Tick
+        For Each x As Control In Controls
+
+            If TypeOf x Is PictureBox AndAlso x.Tag Is "invader" Then
+
+                If CType(x, PictureBox).Bounds.IntersectsWith(pbMe.Bounds) Then
+                    gameOver(OverMethod.LOSE)
+                    removeBullets()
+                    Exit Sub
+                End If
+
+                CType(x, PictureBox).Left += invaderSpeed
+
+                If CType(x, PictureBox).Left > Width + 20 Then
+                    CType(x, PictureBox).Top += CType(x, PictureBox).Height + 10
+                    CType(x, PictureBox).Left = -50
+                End If
+            End If
+        Next
+
+        For Each i As Control In Controls
+
+            For Each j As Control In Controls
+
+                If TypeOf i Is PictureBox AndAlso i.Tag Is "invader" Then
+
+                    If TypeOf j Is PictureBox AndAlso j.Tag Is "bullet" Then
+
+                        If i.Bounds.IntersectsWith(j.Bounds) Then
+                            score += 1
+                            Controls.Remove(i)
+                            Controls.Remove(j)
+                        End If
+                    End If
+                End If
+            Next
+        Next
+        For Each i As Control In Controls
+            For Each j As Control In Controls
+
+                If TypeOf i Is PictureBox AndAlso i.Tag Is "friend" Then
+
+                    If TypeOf j Is PictureBox AndAlso j.Tag Is "bullet" Then
+
+                        If i.Bounds.IntersectsWith(j.Bounds) Then
+                            Controls.Remove(i)
+                            Controls.Remove(j)
+                            gameOver(OverMethod.MURDERER)
+                            Exit Sub
+                        End If
+                    End If
+                End If
+            Next
+        Next
+    End Sub
+
+    Private Sub tmrShields_Tick(sender As Object, e As EventArgs) Handles tmrShields.Tick
+        For Each i As Control In Controls
+
+            For Each j As Control In Controls
+
+                If TypeOf i Is PictureBox AndAlso (i.Tag Is "bomb" Or i.Tag Is "bullet") Then
+
+                    If TypeOf j Is PictureBox AndAlso j.Tag Is "shield" Then
+
+                        If i.Bounds.IntersectsWith(j.Bounds) Then
+                            Controls.Remove(i)
+                            Controls.Remove(j)
+                        End If
+                    End If
+                End If
+            Next
+        Next
+    End Sub
+End Class
+
+Public Enum OverMethod
+    WIN
+    LOSE
+    AMMO
+    MURDERER
+End Enum
---
+++ SpaceInvaders/frmSettings.Designer.vb
@@ -0,0 +1,243 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSettings
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnGo = New System.Windows.Forms.Button()
+        Me.txtBombRate = New System.Windows.Forms.NumericUpDown()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtBombSpeed = New System.Windows.Forms.NumericUpDown()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtInvaderSpeed = New System.Windows.Forms.NumericUpDown()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtPlayerSpeed = New System.Windows.Forms.NumericUpDown()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txtReloads = New System.Windows.Forms.NumericUpDown()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtBulletSpeed = New System.Windows.Forms.NumericUpDown()
+        CType(Me.txtBombRate, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.txtBombSpeed, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.txtInvaderSpeed, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.txtPlayerSpeed, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.txtReloads, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.txtBulletSpeed, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(148, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(111, 45)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Press SPACE to fire" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Use arrows to move" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Press R to reload"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(138, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(135, 30)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "How To Play"
+        '
+        'btnGo
+        '
+        Me.btnGo.Location = New System.Drawing.Point(169, 270)
+        Me.btnGo.Name = "btnGo"
+        Me.btnGo.Size = New System.Drawing.Size(75, 23)
+        Me.btnGo.TabIndex = 2
+        Me.btnGo.Text = "Go"
+        Me.btnGo.UseVisualStyleBackColor = True
+        '
+        'txtBombRate
+        '
+        Me.txtBombRate.Location = New System.Drawing.Point(291, 94)
+        Me.txtBombRate.Maximum = New Decimal(New Integer() {1661992960, 1808227885, 5, 0})
+        Me.txtBombRate.Minimum = New Decimal(New Integer() {50, 0, 0, 0})
+        Me.txtBombRate.Name = "txtBombRate"
+        Me.txtBombRate.Size = New System.Drawing.Size(120, 23)
+        Me.txtBombRate.TabIndex = 3
+        Me.txtBombRate.Value = New Decimal(New Integer() {100, 0, 0, 0})
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(202, 96)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(71, 15)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Bomb Rate: "
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(202, 125)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(80, 15)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Bomb Speed: "
+        '
+        'txtBombSpeed
+        '
+        Me.txtBombSpeed.Location = New System.Drawing.Point(291, 123)
+        Me.txtBombSpeed.Maximum = New Decimal(New Integer() {1661992960, 1808227885, 5, 0})
+        Me.txtBombSpeed.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.txtBombSpeed.Name = "txtBombSpeed"
+        Me.txtBombSpeed.Size = New System.Drawing.Size(120, 23)
+        Me.txtBombSpeed.TabIndex = 5
+        Me.txtBombSpeed.Value = New Decimal(New Integer() {10, 0, 0, 0})
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(202, 154)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(87, 15)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "Invader Speed: "
+        '
+        'txtInvaderSpeed
+        '
+        Me.txtInvaderSpeed.Location = New System.Drawing.Point(291, 152)
+        Me.txtInvaderSpeed.Maximum = New Decimal(New Integer() {1661992960, 1808227885, 5, 0})
+        Me.txtInvaderSpeed.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.txtInvaderSpeed.Name = "txtInvaderSpeed"
+        Me.txtInvaderSpeed.Size = New System.Drawing.Size(120, 23)
+        Me.txtInvaderSpeed.TabIndex = 7
+        Me.txtInvaderSpeed.Value = New Decimal(New Integer() {5, 0, 0, 0})
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(202, 183)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(80, 15)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Player Speed: "
+        '
+        'txtPlayerSpeed
+        '
+        Me.txtPlayerSpeed.Location = New System.Drawing.Point(291, 181)
+        Me.txtPlayerSpeed.Maximum = New Decimal(New Integer() {1661992960, 1808227885, 5, 0})
+        Me.txtPlayerSpeed.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.txtPlayerSpeed.Name = "txtPlayerSpeed"
+        Me.txtPlayerSpeed.Size = New System.Drawing.Size(120, 23)
+        Me.txtPlayerSpeed.TabIndex = 9
+        Me.txtPlayerSpeed.Value = New Decimal(New Integer() {6, 0, 0, 0})
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(202, 241)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(54, 15)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "Reloads: "
+        '
+        'txtReloads
+        '
+        Me.txtReloads.Location = New System.Drawing.Point(291, 239)
+        Me.txtReloads.Maximum = New Decimal(New Integer() {1661992960, 1808227885, 5, 0})
+        Me.txtReloads.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.txtReloads.Name = "txtReloads"
+        Me.txtReloads.Size = New System.Drawing.Size(120, 23)
+        Me.txtReloads.TabIndex = 11
+        Me.txtReloads.Value = New Decimal(New Integer() {3, 0, 0, 0})
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Enabled = False
+        Me.Label8.Location = New System.Drawing.Point(202, 212)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(78, 15)
+        Me.Label8.TabIndex = 14
+        Me.Label8.Text = "Bullet Speed: "
+        '
+        'txtBulletSpeed
+        '
+        Me.txtBulletSpeed.Enabled = False
+        Me.txtBulletSpeed.Location = New System.Drawing.Point(291, 210)
+        Me.txtBulletSpeed.Maximum = New Decimal(New Integer() {1661992960, 1808227885, 5, 0})
+        Me.txtBulletSpeed.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.txtBulletSpeed.Name = "txtBulletSpeed"
+        Me.txtBulletSpeed.Size = New System.Drawing.Size(120, 23)
+        Me.txtBulletSpeed.TabIndex = 13
+        Me.txtBulletSpeed.Value = New Decimal(New Integer() {6, 0, 0, 0})
+        '
+        'frmSettings
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(423, 302)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.txtBulletSpeed)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.txtReloads)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.txtPlayerSpeed)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtInvaderSpeed)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtBombSpeed)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtBombRate)
+        Me.Controls.Add(Me.btnGo)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmSettings"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Space Invaders"
+        CType(Me.txtBombRate, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.txtBombSpeed, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.txtInvaderSpeed, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.txtPlayerSpeed, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.txtReloads, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.txtBulletSpeed, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnGo As Button
+    Friend WithEvents txtBombRate As NumericUpDown
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtBombSpeed As NumericUpDown
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtInvaderSpeed As NumericUpDown
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtPlayerSpeed As NumericUpDown
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txtReloads As NumericUpDown
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtBulletSpeed As NumericUpDown
+End Class
